f8eb3e8bc263b64c883ea0b1cf8b91b917ac7458
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2018-02-08 16:18:21 +0100

    Testing

--- Assignment2/Engine.cs
+++ Assignment2/Engine.cs
@@ -9,19 +9,18 @@ namespace Assignment2
 	{
 		private MainForm form;
 		private Timer timer;
+		ShapeDelegate ShapeDelegate;
 
 		private ISet<Ball> balls = new HashSet<Ball>();
 
 		private Random random = new Random();
 
-		Shape shape = new Shape(400, 300, 100, 200);
-
 		public Engine()
 		{
 			form = new MainForm();
 			timer = new Timer();
-
-			form.MouseClick += MouseClick;
+			ShapeDelegate = new ShapeDelegate(form);
+			form.MouseClick += ShapeDelegate.MClick;
 
 			AddBall();
 		}
@@ -63,40 +62,6 @@ namespace Assignment2
 			{
 				ball.Draw(args.Graphics);
 			}
-			shape.Draw(args.Graphics);
-		}
-
-		private void MouseClick(object sender, MouseEventArgs e)
-		{
-			if (e.Button == System.Windows.Forms.MouseButtons.Right)
-			{
-				var context = new ContextMenuStrip();
-				context.Items.Add("Add SpeedBox", null, new EventHandler(speedBox_add));
-				context.Items.Add("Add SlowBox", null, new EventHandler(slowBox_add));
-				context.Items.Add("Add Horizontal Line", null, new EventHandler(horLine_add));
-				context.Items.Add("Add Vertical Line", null, new EventHandler(verLine_add));
-				context.Show(Cursor.Position);
-			}
-		}
-
-		private void speedBox_add(object sender, EventArgs e)
-		{
-			MessageBox.Show("no");
-		}
-
-		private void slowBox_add(object sender, EventArgs e)
-		{
-			MessageBox.Show("no");
-		}
-
-		private void horLine_add(object sender, EventArgs e)
-		{
-			MessageBox.Show("no");
-		}
-
-		private void verLine_add(object sender, EventArgs e)
-		{
-			MessageBox.Show("no");
 		}
 	}
 }
---
+++ Assignment2/ShapeDelegate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Assignment2
+{
+	class ShapeDelegate : Form
+	{
+		private MainForm form;
+
+		public ShapeDelegate(MainForm form)
+		{
+			this.form = form;
+		}
+
+		public void MClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button == System.Windows.Forms.MouseButtons.Right)
+			{
+				var context = new ContextMenuStrip();
+				context.Items.Add("Add SpeedBox", null, new EventHandler(SpeedBox));
+				context.Items.Add("Add SlowBox", null, new EventHandler(SlowBox));
+				context.Items.Add("Add Horizontal Line", null, new EventHandler(HorizontalLine));
+				context.Items.Add("Add Vertical Line", null, new EventHandler(VerticalLine));
+				context.Show(Cursor.Position);
+			}
+		}
+
+		public void CreateShape()
+		{
+			var label = new Label();
+			label.AutoSize = true;
+			label.Text = "Choose two points.";
+
+			form.Controls.Add(label);
+		}
+
+		private void SpeedBox(object sender, EventArgs e)
+		{
+			CreateShape();
+			this.UseWaitCursor = true;
+		}
+
+		private void SlowBox(object sender, EventArgs e)
+		{
+			CreateShape();
+		}
+
+		private void HorizontalLine(object sender, EventArgs e)
+		{
+			CreateShape();
+		}
+
+		private void VerticalLine(object sender, EventArgs e)
+		{
+			CreateShape();
+		}
+	}
+}
